1003c27280f8ce416bda6e56ba792cca482fca8a
Ivan <inzikin@processtech.ru>
2026-06-03 00:01:26 +0300

    .

--- my-string-builder/src/main/java/Main.java
+++ my-string-builder/src/main/java/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        MyStringBuilder sb = new MyStringBuilder();
 
-        sb.append("Hello ");
-        System.out.println(sb);
-        sb.append("World");
-        System.out.println(sb);
-        sb.undo();
-        System.out.println(sb);
     }
 }
